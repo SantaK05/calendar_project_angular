f1cc07a3e5ebebb64e73f55cfed5c3223063fb2d
Alessio Sarica <alessiosarica05@gmail.com>
2024-12-13 12:56:06 +0100

    View Slots

--- src/app/pages/landing-amministrazione/landing-amministrazione.component.ts
+++ src/app/pages/landing-amministrazione/landing-amministrazione.component.ts
@@ -6,22 +6,56 @@ import {
   RouterLinkActive,
   RouterOutlet,
 } from '@angular/router';
-import { Subject } from 'rxjs';
+import { catchError, map, Subject, switchMap, throwError } from 'rxjs';
+import { AmministrazioneService } from './amministrazione.service';
+import { Slot } from './model/slots.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-landing-amministrazione',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, DatePipe],
   templateUrl: './landing-amministrazione.component.html',
   styleUrl: './landing-amministrazione.component.css',
 })
-export class LandingAmministrazioneComponent implements OnInit, AfterViewInit {
+export class LandingAmministrazioneComponent {
+  arraySlots: Slot[] | undefined;
+
   isSlotView: boolean = false;
   queryEvent: Subject<string> = new Subject();
-  constructor(private router: ActivatedRoute) {}
-  ngAfterViewInit(): void {
-    this.isSlotView =
-      this.router.snapshot.queryParamMap.get('slots') === 'view';
+  constructor(
+    private router: ActivatedRoute,
+    private navigator: Router,
+    private service: AmministrazioneService
+  ) {
+    this.router.queryParamMap
+      .pipe(
+        map((data) => {
+          if (data.get('slots') === 'view') {
+            this.isSlotView = true;
+            this.service.slots
+              .pipe(
+                catchError((err) => {
+                  console.log('Errore', err);
+                  return throwError(err);
+                })
+              )
+              .subscribe((data) => (this.arraySlots = data));
+          } else if (data.get('slots') === null) {
+            this.isSlotView = false;
+            this.arraySlots = undefined;
+          }
+        })
+      )
+      .subscribe();
   }
 
-  ngOnInit(): void {}
+  changeToSlotList(): void {
+    this.navigator.navigate(['/amministrazione'], {
+      queryParams: { slots: 'view' },
+    });
+  }
+
+  changeToHome(): void {
+    this.navigator.navigateByUrl('/amministrazione');
+  }
 }
