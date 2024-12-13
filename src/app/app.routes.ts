9fcec7dd0fec5aa9e348444dc2ca067e10ef9fea
Lancel0tt <anto25.vecchio@gmail.com>
2024-12-13 11:20:09 +0100

    v 1.3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,24 @@ import { Routes } from '@angular/router';
 import { LandingCalendarioComponent } from './pages/landing-calendario/landing-calendario.component';
 import { EmailListComponent } from './pages/landing-email/list/list.component';
 import { TemplateComponent } from './pages/landing-email/template/template.component';
+import { LandingBackofficeComponent } from './pages/landing-backoffice/landing-backoffice.component';
+import { UserComponent } from './pages/landing-backoffice/user/user.component';
+import { ListUserComponent } from './pages/landing-backoffice/user-list/user-list.component';
+import { RoleComponent } from './pages/landing-backoffice/role/role.component';
+import { RoleListComponent } from './pages/landing-backoffice/role-list/role-list.component';
+import { GroupsComponent } from './pages/landing-backoffice/group/group.component';
+import { GroupsListComponent } from './pages/landing-backoffice/group-list/group-list.component';
 
 export const routes: Routes = [
-    { path: '',   redirectTo: '/calendario', pathMatch: 'full' },
-    { path: 'calendario', component: LandingCalendarioComponent },
-    { path: 'email-list', component: EmailListComponent },
-    { path: 'template', component: TemplateComponent },
-
+  { path: '', redirectTo: '/calendario', pathMatch: 'full' },
+  { path: 'calendario', component: LandingCalendarioComponent },
+  { path: 'email-list', component: EmailListComponent },
+  { path: 'template', component: TemplateComponent },
+  { path: 'backoffice', component: LandingBackofficeComponent },
+  { path: 'backoffice/users', component: ListUserComponent },
+  { path: 'backoffice/users/:id', component: UserComponent },
+  { path: 'backoffice/roles', component: RoleListComponent },
+  { path: 'backoffice/roles/:id', component: RoleComponent },
+  { path: 'backoffice/groups', component: GroupsListComponent },
+  { path: 'backoffice/groups/:id', component: GroupsComponent },
 ];
--- src/app/pages/landing-backoffice/role/role.component.ts
+++ src/app/pages/landing-backoffice/role/role.component.ts
@@ -1,11 +1,61 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RoleService } from '../role.service';
+import { Role } from '../../../interfaces/backoffice';
+import { MessageService } from '../message.service';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { catchError, switchMap, tap, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-role',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './role.component.html',
-  styleUrl: './role.component.css'
+  styleUrl: './role.component.css',
 })
 export class RoleComponent {
+  current: Role = {
+    id: 0,
+    nome: 'nome',
+    descrizione: 'descrizione',
+  };
 
+  triggerNavigation: boolean = false;
+
+  constructor(
+    private service: RoleService,
+    private messageService: MessageService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.service
+        .load(id)
+        .pipe(
+          tap((data) => {
+            console.log('ottenuti dati ruoli');
+            console.log(data);
+            this.current = data as Role;
+          }),
+          catchError((err) => {
+            console.log(err);
+            this.messageService.publishError('Errore caricamento dati.');
+            return throwError(() => err);
+          })
+        )
+        .subscribe();
+    }
+  }
+
+  save() {
+    this.service.save(this.current).pipe(
+      switchMap((response) => {
+        return this.service.findAll();
+      }),
+      tap((data: any) => {
+        this.router.navigateByUrl('/list-role');
+      })
+    );
+  }
 }
