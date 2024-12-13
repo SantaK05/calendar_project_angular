5109629936860829cd4e79afc9071f7517c3502b
Alessio Sarica <alessiosarica05@gmail.com>
2024-12-13 11:39:14 +0100

    aiwzdge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { LandingCalendarioComponent } from './pages/landing-calendario/landing-calendario.component';
 import { EmailListComponent } from './pages/landing-email/list/list.component';
 import { TemplateComponent } from './pages/landing-email/template/template.component';
+import { LandingAmministrazioneComponent } from './pages/landing-amministrazione/landing-amministrazione.component';
 
 export const routes: Routes = [
-    { path: '',   redirectTo: '/calendario', pathMatch: 'full' },
-    { path: 'calendario', component: LandingCalendarioComponent },
-    { path: 'email-list', component: EmailListComponent },
-    { path: 'template', component: TemplateComponent },
-
+  { path: '', redirectTo: '/calendario', pathMatch: 'full' },
+  { path: 'calendario', component: LandingCalendarioComponent },
+  { path: 'email-list', component: EmailListComponent },
+  { path: 'template', component: TemplateComponent },
+  { path: 'amministrazione', component: LandingAmministrazioneComponent },
+  {
+    path: 'amministrazione?slots=view',
+    component: LandingAmministrazioneComponent,
+  },
 ];
--- src/app/pages/landing-amministrazione/landing-amministrazione.component.ts
+++ src/app/pages/landing-amministrazione/landing-amministrazione.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-landing-amministrazione',
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './landing-amministrazione.component.html',
-  styleUrl: './landing-amministrazione.component.css'
+  styleUrl: './landing-amministrazione.component.css',
 })
-export class LandingAmministrazioneComponent {
+export class LandingAmministrazioneComponent implements OnInit, AfterViewInit {
+  isSlotView: boolean = false;
+  queryEvent: Subject<string> = new Subject();
+  constructor(private router: ActivatedRoute) {}
+  ngAfterViewInit(): void {
+    this.isSlotView =
+      this.router.snapshot.queryParamMap.get('slots') === 'view';
+  }
 
+  ngOnInit(): void {}
 }
