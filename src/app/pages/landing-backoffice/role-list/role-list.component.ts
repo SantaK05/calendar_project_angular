44635b63cb8343a4136ee6f362f7d4aa3b9bec46
Lancel0tt <anto25.vecchio@gmail.com>
2025-01-17 12:49:02 +0100

    v 3.8.2

--- src/app/pages/landing-backoffice/role-list/role-list.component.ts
+++ src/app/pages/landing-backoffice/role-list/role-list.component.ts
@@ -2,17 +2,30 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { switchMap } from 'rxjs';
 import { Role } from '../../../interfaces/backoffice';
-import { NavbarComponent } from "../navbar/navbar.component";
+import { NavbarComponent } from '../navbar/navbar.component';
 import { RoleService } from '../services/role.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-role',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FormsModule, CommonModule],
   templateUrl: './role-list.component.html',
   styleUrl: './role-list.component.css',
 })
 export class ListRoleComponent {
+  role: any[] = [];
+  filteredRole: any[] = [];
+  searchTerm: string = '';
+
   constructor(private service: RoleService, private router: Router) {}
+
+  ngOnInit() {
+    this.service.findAll().subscribe((data: Role[]) => {
+      this.role = data;
+      this.filteredRole = data;
+    });
+  }
 
   get arrayRole() {
     return this.service.arrayRole;
@@ -27,6 +40,17 @@ export class ListRoleComponent {
 
     if (result) {
       result.pipe(switchMap(() => this.service.findAll())).subscribe();
+    }
+  }
+
+  searchRole() {
+    const term = this.searchTerm.toLowerCase();
+    if (term) {
+      this.filteredRole = this.role.filter((role) =>
+        role.nome.toLowerCase().includes(term)
+      );
+    } else {
+      this.filteredRole = this.role;
     }
   }
 }
